0119cee7621faf86a9c21be250b9aa23a0cc3143
Vytautas Lozickas <v.lozickas@gmail.com>
2019-07-17 21:59:04 +0300

    Install and configure eslint and prettier

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
+
 import styles from './App.scss';
 
 export class App extends React.Component {
-    render() {
-        return (
-            <div className={styles.App}>Hello, world!</div>
-        )
+    public render(): React.ReactNode {
+        return <div className={styles.App}>Hello, world!</div>;
     }
 }
